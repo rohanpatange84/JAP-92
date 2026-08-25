8255feddc82483cdc9e7e73dcfc9b788be384765
Rohan Patange <rohanpatange84@gmail.com>
2026-08-25 22:49:07 +0530

    class object

--- Class_Object/NumberProject/src/App.java
+++ Class_Object/NumberProject/src/App.java
@@ -14,7 +14,15 @@ public class App{
 		System.out.println("The count of number is : "+n1.countDigit());
 		System.out.println("The last digit of number is : "+n1.lastDigit());
 		System.out.println("The first digit of number is : "+n1.firstDigit());
-		System.out.println("The sum of first and last digit of number is : "+n1.sumOfFirstAndLastDigit(n1.firstDigit(),n1.lastDigit()));
+		System.out.println("The sum of first and last digit of number is : "+n1.sumOfFirstAndLastDigit());
+		System.out.println("The sum of digit  : "+n1.sumOfDigit());
+		System.out.println("The product of digit  : "+n1.productOfDigit());
+		System.out.println("Reverse number is  : "+n1.reverseNum());
 
+		boolean res = n1.isPelindrome();
+		if(res)
+			System.out.println("Number is Palindrome");
+		else 
+			System.out.println("number is not palindrome");
 	}
 }
--- Class_Object/NumberProject/src/NumberOpe.java
+++ Class_Object/NumberProject/src/NumberOpe.java
@@ -30,9 +30,51 @@ public class NumberOpe{
 	}
 
 	//Find the sum of the first and last digit of any number
-	public int sumOfFirstAndLastDigit(int num1,int num2){
-	    return num1+num2;
+	public int sumOfFirstAndLastDigit(){
+	    return lastDigit()+firstDigit();
 	}
 
+	//Calculate the sum of digits of any number
+	public int sumOfDigit(){
+		int temp=num;
+		int sum=0;
+		while(temp>0){
+			sum=sum+temp%10;
+			temp=temp/10;
+		}
+		return sum;
+	}
+
+	//Calculate the product of digits of any number
+	public int productOfDigit(){
+		int temp=num;
+		int mul=1;
+		while(temp>0){
+			mul=mul*(temp%10);
+			temp=temp/10;
+
+		}
+		return mul; 
+	}
+
+	//Reverse a given number
+	public int reverseNum(){
+		int temp=num;
+		int newnum=0;
+		while(temp>0){
+			newnum=newnum*10+temp%10;
+			temp=temp/10;
+		}
+		return newnum;
+	}
+
+	//Check whether a number is palindrome or not
+	public boolean isPelindrome(){
+		int rev=reverseNum();
+		if (rev==num) 
+			return true;
+		else 
+			return false;
+	}
 
 }
