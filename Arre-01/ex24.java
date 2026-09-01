d0fe3d47cd94f563c9302fa011125e4c12c4007b
Rohan Patange <rohanpatange84@gmail.com>
2026-09-01 16:28:59 +0530

    array'

--- Arre-01/ex24.java
+++ Arre-01/ex24.java
@@ -24,10 +24,20 @@ public  class ex24{
 	}
 
 	public static void printDigit(int arr[]){
+
+		for(int j=0;j<arr.length;j++){
+			arr[j]=getAverage(arr[j]);
+		}
+
 		System.out.print("Average : ");
 		for(int i=0;i<arr.length;i++){
-			System.out.print(" "+getAverage(arr[i]));
+			System.out.print(" "+(arr[i]));
 		}
+
+		
+
+		
+		
 	}
 
 	public static void main(String[] args) {
