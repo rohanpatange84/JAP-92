f89c405dfdc150bbfeba44bb6e250c43d9cfbd39
Rohan Patange <rohanpatange84@gmail.com>
2026-09-02 15:33:32 +0530

    ss

--- Arre-02/StudentProject2/src/App.java
+++ Arre-02/StudentProject2/src/App.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 public class App{
 	public static void main(String[] args) {
-		Student s1[]=new Student[3];
+		Student s1[]=new Student[2];
 		Scanner sc = new Scanner(System.in);
 
 		for(int i=0;i<s1.length;i++){
 			System.out.print("Enter Name: ");
 			String name=sc.nextLine();
-			s1[i]=new Student(name);
 			sc.nextLine();
+			s1[i]=new Student(name);
+			
 			for(int j=0;j<5;j++){
 				System.out.print("Enter Mark: ");
 				int mark=sc.nextInt();
@@ -17,9 +18,12 @@ public class App{
 			}
 		}
 
-		for(int i=0;i<s1.length;i++){
-			System.out.println(s1[i].getName());
+		for(int i=0;i<5;i++){
+			System.out.println(s1[0].marks[i]);
 		}
+
+
+
 
 
 		
